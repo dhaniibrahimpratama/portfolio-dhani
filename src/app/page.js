94de0a6d9b1dbbc0d65fd398c707591b6cb36451
Dhani Ibrahim Pratama <ibrahimpratama2005@gmail.com>
2026-01-15 01:04:05 +0700

    Update tampilan gambar jadi rapi

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
 
       <div className="max-w-5xl mx-auto">
         <h1 className="text-4xl font-bold mb-8 text-center">
-          Portfolio Dhani (Suka Ngoding) Ibrahim
+          Dhani (Suka Ngoding) Ibrahim
         </h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6" id="projects">
